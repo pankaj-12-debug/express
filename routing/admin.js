5871f53e18b1326b700f0c6550039fe58daf2272
pankaj-12-debug <kajopkkumar12@gmail.com>
2022-10-20 13:46:05 +0530

    router-folder

---
+++ routing/admin.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const router=express.Router();
+router.get('/add-product',(req,res,next)=>{
+    console.log('bale');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="text" name="object"><button type="submit">add product</button></form>')
+})
+router.post('/add-product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/shop');
+})
+module.exports=router;
