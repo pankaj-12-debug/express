0c7c7f4fe464a1a16970ab57b0c5c4c6052d7639
pankaj-12-debug <kajopkkumar12@gmail.com>
2022-10-20 10:46:20 +0530

    body-parser

--- attach.js
+++ attach.js
@@ -1,16 +1,28 @@
 //const http=require('http');
 //const router=require('./router.js');
-const express=require('express');
+const express =require('express');
+const bodyParser=require('body-parser');
+
 const app=express();
-app.use((req,res,next)=>{
-    console.log('hello');
-    next();
-});
-app.use((req,res,next)=>{
-    console.log('hello world');
-    res.send('{ key1: value }');
-});
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/about',(req,res,next)=>{
+    console.log('us');
+    res.send('<h1>"About" page</h1>')
+})
+app.use('/add-product',(req,res,next)=>{
+    console.log('bale');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="object"><button type="submit">add product</button></form>')
+})
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
+    console.log('maza');
+    res.send('<h1>hello express</h1>')
+})
+
 //const server=http.createServer(router);
 //const server=http.createServer(app);
 //server.listen(4500);
-app.listen(4500);
+app.listen(3000);
