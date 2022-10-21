4902e667171d987e92c3d97dc69efe37b81ce83d
pankaj-12-debug <kajopkkumar12@gmail.com>
2022-10-21 13:09:12 +0530

    new-path

--- routing/admin.js
+++ routing/admin.js
@@ -1,9 +1,13 @@
+const path=require('path');
 const express=require('express');
+const root=require('../routing/util/path');
 const router=express.Router();
 router.get('/add-product',(req,res,next)=>{
     console.log('bale');
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="text" name="object"><button type="submit">add product</button></form>')
+   // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="text" name="object"><button type="submit">add product</button></form>')
+res.sendFile(path.join(root,'view','add-product.html'));
 })
+
 router.post('/add-product',(req,res,next)=>{
     console.log(req.body);
     res.redirect('/shop');
