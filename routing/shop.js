6c1051fdc4f05f3c15f5b3504a6a7a27a55fac95
pankaj-12-debug <kajopkkumar12@gmail.com>
2022-10-21 13:11:01 +0530

    shop

--- routing/shop.js
+++ routing/shop.js
@@ -1,7 +1,8 @@
+const path=require('path');
 const express=require('express');
 const router=express.Router()
 router.get('/',(req,res,next)=>{
     console.log('maza');
-    res.send('<h1>hello express</h1>')
+    res.sendFile(path.join(__dirname,'../','view','shop.html'))
 });
 module.exports = router;
