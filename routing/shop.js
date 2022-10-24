2b25cc150bfd3cfcbb180e9c1a2e41f3df9e2873
pankaj-12-debug <kajopkkumar12@gmail.com>
2022-10-24 13:32:47 +0530

    routing-shop

--- routing/shop.js
+++ routing/shop.js
@@ -1,8 +1,10 @@
-const path=require('path');
+//const path=require('path');
 const express=require('express');
 const router=express.Router()
-router.get('/',(req,res,next)=>{
-    console.log('maza');
-    res.sendFile(path.join(__dirname,'../','view','shop.html'))
-});
+const shopController=require('../controllers/shop');
+router.get('/',shopController.shopProduct);
+//router.get('/',(req,res,next)=>{
+    //console.log('maza');
+  //  res.sendFile(path.join(__dirname,'../','view','shop.html'))
+//});
 module.exports = router;
